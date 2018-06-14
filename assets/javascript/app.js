5ec951d4842f7f6a6982f71ef2a4acf07ef6a7a7
Michelangelo <youngbrennin20@gmail.com>
2018-06-14 15:10:49 -0700

    Added button maker and spawn gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,10 @@
-$("#searchButton").on("click", function () {
+var gifList = [];
 
+function createStuff() {
+
+    var fillGif = $(this).attr("data-name");
     var APIkey = "0emzCKL1a7sCMkjMLF5Mz0M4N4WxiMUE"
-
-    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=" + APIkey + "&tag=" + "deadpool";
+    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=" + APIkey + "&tag=" + fillGif;
 
     $.ajax({
         url: queryURL,
@@ -12,12 +14,34 @@ $("#searchButton").on("click", function () {
         var newImage = $('<img>');
         newImage.attr("src", imageUrl);
         newImage.attr("alt", "random image");
-        $('#images').prepend(newImage);
+        $('#imageArea').prepend(newImage);
     });
+};
+
+function addButton() {
+
+    $("#buttonArea").empty();
+
+    for (var i = 0; i < gifList.length; i++) {
+
+        var makeButton = $("<button>");
+        makeButton.addClass("gif-btn");
+        makeButton.attr("data-name", gifList[i]);
+        makeButton.text(gifList[i]);
+        $("#buttonArea").append(makeButton);
+    }
+}
+
+$("#searchButton").on("click", function (event) {
+    event.preventDefault();
+    var fillGif = $("#gifInput").val().trim();
+    gifList.push(fillGif);
+    addButton();
 });
 
+$(document).on("click", ".gif-btn", createStuff);
 
-
+addButton();
 
 
 
